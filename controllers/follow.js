5da67bb2599c153a03b7ace5db12861eabbd6212
ryanpedersen42 <ryanpedersen42@gmail.com>
2019-02-18 10:42:57 -0800

    handle follow button

--- controllers/follow.js
+++ controllers/follow.js
@@ -2,19 +2,46 @@ const handleFollow = (req, res, db) => {
   const { username, following } = req.body;
 
   if (!username || !following ) {
-    return res.status(400).json(` ${req.body.username} ${req.body.following} in incorrect form submission`);
+    return res.status(400).json(`incorrect form submission`);
   }
 
-  db.transaction(trx => {
-    trx.insert({
-      username: username,
-      following: following
+  db.select('following').from('following').where('username', '=', username)
+  .then(responseData => {
+    const arrayThis = responseData.map(e => e.following)
+    const findNumber = arrayThis.find(e => e === Number(req.body.following))
+
+    if (findNumber) {
+        removeFollowing(username, following)
+      } else {
+        addNumber(username, following)
+      }
     })
-    .into('following')
-    .then(trx.commit)
-    .catch(trx.rollback)
-    })
-  .catch(err => res.status(400).json('unable to follow'))
+
+
+  const addNumber = (username, following) => {
+    db.transaction(trx => {
+      trx.insert({
+        username: username,
+        following: following
+      })
+      .into('following')
+      .then(trx.commit)
+      .catch(trx.rollback)
+    })      
+  }
+
+  const removeFollowing = (username, following) => {
+      db.transaction(trx => {
+        trx.del()        
+        .from('following')
+        .where({
+          username: username,
+          following: following
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
+      })
+    }
 }
 
 module.exports = {
